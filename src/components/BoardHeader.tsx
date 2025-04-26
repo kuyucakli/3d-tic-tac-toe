875db479a2af5d0a75fbaaf9fc2302386178ed0a
Burak Kuyucaklı <burak.kuyucakli@gmail.com>
2025-04-26 15:29:35 +0300

    Add current player display animation

--- src/components/BoardHeader.tsx
+++ src/components/BoardHeader.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Move } from '../types/index.dt.ts';
+import styles from './Board.module.css';
 
 interface BoardHeaderProps {
     currentMove: Move;
@@ -8,7 +9,19 @@ interface BoardHeaderProps {
 const BoardHeader: React.FC<BoardHeaderProps> = ({ currentMove }) => (
     <header className="margin-center">
         <h2 className="headlinesmall">
-            Player <span style={{ color: "var(--clr-accent-1" }}><span>{Move[currentMove]}</span></span>
+            Player
+
+            <span className={styles.CurrentPlayerDisplay}>
+                <span className={styles.CurrentPlayerDisplayActive}>{Move[currentMove]}
+                </span>
+                <ul key={Math.random()}>
+                    <li>🔥</li>
+                    <li>💀</li>
+                    <li>🚀</li>
+                    <li>🎉</li>
+                    <li>😬</li>
+                </ul>
+            </span>
         </h2>
     </header>
 );
